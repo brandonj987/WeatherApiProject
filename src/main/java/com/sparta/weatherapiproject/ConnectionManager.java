7a65dcf03793827faba271cb97f29fbd03f43fb6
BwongmcST <ben.manchunwong@gmail.com>
2022-02-23 15:56:16 +0000

    updated Connection Manager

--- src/main/java/com/sparta/weatherapiproject/ConnectionManager.java
+++ src/main/java/com/sparta/weatherapiproject/ConnectionManager.java
@@ -1,24 +1,23 @@
 package com.sparta.weatherapiproject;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sparta.weatherapiproject.jackson.WeatherReport;
-
 import java.io.IOException;
 import java.net.URL;
 
 public class ConnectionManager {
 
-    public static WeatherReport weatherReportConnection(String city, String apiKey){
-        WeatherReport weatherReport = new WeatherReport();
+    public static CurrentWeatherData currentWeatherDataConnection(String city, String apiKey){
+        CurrentWeatherData currentWeatherData = new CurrentWeatherData();
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            weatherReport = objectMapper.readValue(
+            currentWeatherData = objectMapper.readValue(
                     new URL("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey),
-                    WeatherReport.class);
+                    CurrentWeatherData.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return weatherReport;
+        return currentWeatherData;
     }
+
 
 }
