e7ca19fc0fb5533118281b3628cfcd13d165eed4
AntonyA3 <41267884+AntonyA3@users.noreply.github.com>
2022-02-24 11:36:33 +0000

    Added the Rain and Snow to jackson weather

--- src/main/java/com/sparta/weatherapiproject/jackson/Clouds.java
+++ src/main/java/com/sparta/weatherapiproject/jackson/Clouds.java
@@ -7,7 +7,19 @@ public class Clouds{
 	@JsonProperty("all")
 	private int all;
 
+	public void setAll(int all){
+		this.all = all;
+	}
+
 	public int getAll(){
 		return all;
 	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Clouds{" + 
+			"all = '" + all + '\'' + 
+			"}";
+		}
 }
--- src/main/java/com/sparta/weatherapiproject/jackson/Coord.java
+++ src/main/java/com/sparta/weatherapiproject/jackson/Coord.java
@@ -5,16 +5,33 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Coord{
 
 	@JsonProperty("lon")
-	private int lon;
+	private double lon;
 
 	@JsonProperty("lat")
 	private double lat;
 
-	public int getLon(){
+	public void setLon(double lon){
+		this.lon = lon;
+	}
+
+	public double getLon(){
 		return lon;
+	}
+
+	public void setLat(double lat){
+		this.lat = lat;
 	}
 
 	public double getLat(){
 		return lat;
 	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Coord{" + 
+			"lon = '" + lon + '\'' + 
+			",lat = '" + lat + '\'' + 
+			"}";
+		}
 }
