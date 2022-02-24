4c77a02a256b020dec8ec9f7f105fc10d098349f
Autumn Pelešová <autumnlara@hotmail.com>
2022-02-24 10:42:54 +0000

    Added test json file, testGetClouds method

--- src/main/test/InjectorTest.java
+++ src/main/test/InjectorTest.java
@@ -1,16 +1,18 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.weatherapiproject.ApiKeyGetter;
 import com.sparta.weatherapiproject.WeatherInjector;
-import com.sparta.weatherapiproject.ConnectionManager;
 import com.sparta.weatherapiproject.jackson.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class InjectorTest {
-    private static final String TEST_URL = "";
+    private static final String TEST_JSON_PATH = "../../../test.json";
     private static final double TEST_COORD_LON = 0;
     private static final double TEST_COORD_LAT = 0;
     private static final int TEST_WIND_DEG = 0;
@@ -18,15 +20,14 @@ public class InjectorTest {
     private static final double TEST_WIND_SPEED = 0;
     private static final int TEST_SYS_SUNRISE = 0;
     private static final int TEST_SYS_SUNSET = 0;
+    private static final int TEST_CLOUDS_ALL = 0;
 
     private static WeatherInjector wi;
-    private static String apiKey;
     private static String jsonString;
 
     @BeforeAll
-    public static void setUp() {
+    public static void setUp() throws IOException {
         wi = new WeatherInjector();
-        apiKey = ApiKeyGetter.getApiKey();
         jsonString = getJSONString();
     }
 
@@ -51,6 +52,14 @@ public class InjectorTest {
 
     @Test
     @DisplayName("Given a JSON Object, the injector should be able to retrieve" +
+            "the clouds data and store it in a Clouds object.")
+    public void testGetClouds() throws JsonProcessingException {
+        Clouds clouds = wi.getClouds(jsonString);
+        Assertions.assertEquals(clouds.getAll(), TEST_CLOUDS_ALL);
+    }
+
+    @Test
+    @DisplayName("Given a JSON Object, the injector should be able to retrieve" +
             "the sun cycle data and store it in a Sys object.")
     public void testGetSunCycle() throws JsonProcessingException {
         Sys sys = wi.getSunCycle(jsonString);
@@ -58,12 +67,8 @@ public class InjectorTest {
         Assertions.assertEquals(sys.getSunset(), TEST_SYS_SUNSET);
     }
 
-    private static String getJSONString() {
-        String s = ConnectionManager.httpConnection(TEST_URL, apiKey);
-        if(s != null) return s; // else
-        ConnectionManager cm = Mockito.mock(ConnectionManager.class);
-        Mockito.when(cm.httpConnection(TEST_URL, apiKey)).thenReturn("test");
-        return cm.httpConnection(TEST_URL, apiKey);
+    private static String getJSONString() throws IOException {
+        return new String(Files.readAllBytes(Paths.get(TEST_JSON_PATH)));
     }
 
 }
