197da99dfa625fb99d6bb13cdaf08cbbdfb91eee
AntonyA3 <41267884+AntonyA3@users.noreply.github.com>
2022-02-25 11:19:02 +0000

    Removed commented code from Weather Injector and weather Injector Test

--- src/main/java/com/sparta/weatherapiproject/WeatherInjector.java
+++ src/main/java/com/sparta/weatherapiproject/WeatherInjector.java
@@ -21,43 +21,5 @@ public class WeatherInjector {
         String url = String.format("https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s", lat, lon, apiKey);
         return new ObjectMapper().readValue(new URL(url), CurrentWeatherData.class);
     }
-//
-//    public Coord getCoordinates(String jsonString) throws JsonProcessingException{
-//        return new ObjectMapper().readValue(jsonString, Coord.class);
-//    }
-//    public List<WeatherItem> getWeatherList(String jsonString) throws JsonProcessingException {
-//        return new ObjectMapper().readValue(jsonString, new TypeReference<List<WeatherItem>>(){});
-//    }
-//
-//    public WeatherItem getWeatherItem(String jsonString) throws JsonProcessingException{
-//        return new ObjectMapper().readValue(jsonString, WeatherItem.class);
-//    }
-//
-//    public Wind getWind(String jsonString) throws JsonProcessingException {
-//        return new ObjectMapper().readValue(jsonString, Wind.class);
-//    }
-//
-//    public Clouds getClouds(String jsonString) throws JsonProcessingException{
-//        return new ObjectMapper().readValue(jsonString, Clouds.class);
-//    }
-//
-//    public Rain getRain(String jsonString) throws JsonProcessingException{
-//        return new ObjectMapper().readValue(jsonString, Rain.class);
-//    }
-//
-//    public Snow getSnow(String jsonString) throws JsonProcessingException{
-//        return new ObjectMapper().readValue(jsonString, Snow.class);
-//    }
-//
-//    public Sys getSunCycle(String jsonString) throws  JsonProcessingException{
-//        return new ObjectMapper().readValue(jsonString, Sys.class);
-//    }
-//
-//
-//    public Main getMain(String jsonString) throws JsonProcessingException {
-//        return new ObjectMapper().readValue(jsonString, Main.class);
-//    }
-
-
 
 }
