a79a67803c95395aaf85c39d560a66c83c1dbbaf
Autumn Pelešová <autumnlara@hotmail.com>
2022-02-25 17:33:55 +0000

    Added more BDD step defs

--- src/test/java/com/sparta/weatherapiproject/jackson/CurrentWeatherDataStepDefs.java
+++ src/test/java/com/sparta/weatherapiproject/jackson/CurrentWeatherDataStepDefs.java
@@ -1,5 +1,6 @@
 package com.sparta.weatherapiproject.jackson;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -11,10 +12,15 @@ import java.util.List;
 public class CurrentWeatherDataStepDefs {
     private CurrentWeatherData cwd;
     private Object obj;
+    private int integer;
 
-    @Given("I have Current Weather Data for clear weather")
-    public void iHaveCurrentWeatherDataForClearWeather() {
+    @Given("I have Current Weather Data")
+    public void iHaveCurrentWeatherData() {
         cwd = new CurrentWeatherData();
+    }
+
+    @When("The weather is clear")
+    public void theWeatherIsClear() {
         List<WeatherItem> wList = new ArrayList<>();
         WeatherItem wi = new WeatherItem();
         wi.setId(800);
@@ -41,8 +47,29 @@ public class CurrentWeatherDataStepDefs {
         obj = cwd.getSnow();
     }
 
-    @Then("I should expect the object reference to be null")
-    public void iShouldExpectTheObjectReferenceToBeNull() {
+    @When("I get the Timezone")
+    public void iGetTheTimezone() {
+        integer = cwd.getTimezone();
+    }
+
+    @Then("I should expect it to be null")
+    public void iShouldExpectItToBeNull() {
         Assertions.assertNull(obj);
+    }
+
+    @When("I get Weather")
+    public void iGetWeather() {
+        obj = cwd.getWeather();
+    }
+
+    @Then("The integer value should be between {int} and {int}")
+    public void theIntegerValueShouldBeBetweenAnd(int min, int max) {
+        Assertions.assertTrue(integer >= min);
+        Assertions.assertTrue(integer <= max);
+    }
+
+    @Then("I should expect it not to be null")
+    public void iShouldExpectItNotToBeNull() {
+        Assertions.assertNotNull(obj);
     }
 }
--- src/test/resources/Rain.feature
+++ src/test/resources/Rain.feature
@@ -3,5 +3,5 @@ Feature: Rain
   @getRainIn1Hour
   Scenario: Get Rain in 1 hour
     Given I have rain data
-    When I get the rain in 1 hour
+    When I get the rain over the past hour
     Then I should get the rain in the hour
